a384b013623df3ac7a6582eb2521184f8d1b26ab
SDI <vkflskfdk1@naver.com>
2022-09-20 11:20:21 +0900

    InfoPwCheckService => PwCheckToReviseInfoService 이름 변경 servlet mapping 변경

--- Classcendo/src/main/java/Controller/PwCheckToReviseInfoService.java
+++ Classcendo/src/main/java/Controller/PwCheckToReviseInfoService.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpSession;
 import Model.UserInfoDAO;
 import Model.UserInfoDTO;
 
-public class InfoPwCheckService extends HttpServlet {
+public class PwCheckToReviseInfoService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 인코딩
