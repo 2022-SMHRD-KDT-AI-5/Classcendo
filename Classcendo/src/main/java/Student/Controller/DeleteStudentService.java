4ebfde51c927452a58c7ca6f476b7facd720c51a
SDI <vkflskfdk1@naver.com>
2022-10-01 15:20:07 +0900

    ReviseStudent.jsp 수정

--- Classcendo/src/main/java/Student/Controller/DeleteStudentService.java
+++ Classcendo/src/main/java/Student/Controller/DeleteStudentService.java
@@ -2,18 +2,15 @@ package Student.Controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Student.Model.StudentRecordDAO;
-import Student.Model.StudentRecordDTO;
 
-public class DeleteStudentRecordService extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+public class DeleteStudentService extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 인코딩
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
@@ -21,12 +18,13 @@ public class DeleteStudentRecordService extends HttpServlet {
 		
 		// Parameter 호출
 		int srSeq = Integer.parseInt(request.getParameter("srSeq"));
+		String srlSeq = request.getParameter("srlSeq");
 
 		// StudentRecordDTO, StudentRecordDAO 호출
 		StudentRecordDAO dao = new StudentRecordDAO();
+		
+		System.out.println("asdf"+srlSeq);
 		boolean result = dao.deleteStudentRecord(srSeq);
-
-		PrintWriter out = response.getWriter();
-		out.print(result);
+		response.sendRedirect("UpdateGetStudentListToReviseService?result=" + result + "&srlSeq=" + srlSeq);
 	}
 }
